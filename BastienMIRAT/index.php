8005985ef4d7c553759859ffd2968699cda66040
Bastien-Mirat <32570673+Bastien-Mirat@users.noreply.github.com>
2017-11-15 20:25:51 +0100

    pour rendre un peu jolie

--- BastienMIRAT/index.php
+++ BastienMIRAT/index.php
@@ -2,53 +2,58 @@
 <h1>Welcome to DWM</h1>  
 <script src="https://d3js.org/d3.v4.min.js"></script>
 <script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
-<body>
-		<div id="intro">
-			Présentation des étudiants en L3 Design Web Mobile.
-		</div>
-		<div id="content">
-			<ul class="photo" id="lstPhoto" >
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- personal css file -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="http://dimplejs.org/dist/dimple.v2.3.0.min.js"></script>
+
+
+<body style="background-color: #e0e2bf;">
+    <div class="wrapper">
+       
+        <div class="centerorange"><h5 class="white">Année universitaire 2017-2018 </h5> </div>
+        <br><br>
+
+        <div class="container">
+            <div class="row" id="etuCards" >
+            </div>
+        </div>
 				
-				<script>
-				// Obtention de l'API.
-				$.getJSON("https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=79672885f8a018343cd8849f57e8a50a&photoset_id=72157686701939221&extras=original_format&format=json&jsoncallback=?",
-				function(data){
-					
-				    // Debut de la boucle
-				    $.each(data.photoset.photo, function(i,item){
-				        // Sockage de l'image dans une variable
-				        var photo = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_n.jpg';
-				        // Sockage de l'url dans une variable
-				        var url= 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_c.jpg';
-				        // Affichage des images dans la balise ul#images avec le l'url dans la balise li
-				      $("<img/>").attr({src: photo, alt: item.title}).appendTo("#lstPhoto").wrap("<li><a href=' "+ url +"' title=' "+ item.title +" ' ></a><span id='data"+i+"'/></li>");
-				 
-				    }); //Fin de la boucle
+				 <script>
+            var dataPhoto;
+            
+            var urlFlickr = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=79672885f8a018343cd8849f57e8a50a&photoset_id=72157686701939221&extras=original_format&format=json&jsoncallback=?";
+            $.getJSON(urlFlickr,
+            function(data){
+                dataPhoto = data;
+                var url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT4vIu2pcoPH6Vng35aQzwRAxQV9ogt8B9fyp-3cr1aVEdEh5rAhDfj_sB7OYoeLRZGalhq1trkCLRd/pub?gid=2097717787&single=true&output=csv';
+                d3.csv(url, function(data) {
+                    var cards = d3.select('#etuCards').selectAll(".col-sm-3 col-md-4").data(data).enter()
+                        .append('div').attr('class','col-sm-3 mb-4')
+                        .append("div").attr('class','card');
+                    cards.append("img")
+                            .attr('class','card-img-top hvr-bounce-in')
+                            .attr('src',function(d) {
+                                var item = dataPhoto.photoset.photo[d['num photo']];
+                                var src = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_c.jpg';
+                                return src;
+                            });
+                    var cardBody = cards.append('div').attr('class','card-body');
+                    cardBody.append('h4').attr('class','card-title')
+                                .text(function(d) {
+                                    return d['Votre prénom'].substr(0,1).toUpperCase() +d['Votre prénom'].substr(1).toLowerCase() + '  ' + d['Votre nom'].toUpperCase();
+                                });
+                    cards.append('div').attr('class','card-footer')
+                        .append('small').attr('class','text-muted')
+                        .text(function(d) {
+                            return d['Votre mail'].toLowerCase();
+                        });
+                });
+            });
+        </script>
 
-
-				  //récupération des données du formulaire google
-					var url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT4vIu2pcoPH6Vng35aQzwRAxQV9ogt8B9fyp-3cr1aVEdEh5rAhDfj_sB7OYoeLRZGalhq1trkCLRd/pub?gid=2097717787&single=true&output=csv';
-				    //var url = "data/LPDesignWebMobile_17-18.csv";        
-					d3.csv(url, function(data) {
-						dataForm = data;
-
-						dataForm.forEach(function(d){
-							var np = d['num photo'];
-							var nom = d['Votre nom'];	
-							var prenom = d['Votre prénom'];	
-							d3.select("#data"+np)
-							.text(prenom+'  '+nom);
-						});
-						
-						
-						});		
-						
-				    
-				});
-				</script>
-			</ul>
-		</div>
-		
+			</div>
+	
 	</body>
 </div>
 
